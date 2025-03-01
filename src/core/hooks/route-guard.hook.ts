90929787a94792f17d74e73ba16ddaf72c1eee5c
Pedro Silva <pedro.silva-dev@hotmail.com>
2025-03-01 11:50:57 -0300

    refactor: remove query string from validation in route guard

--- src/core/hooks/route-guard.hook.ts
+++ src/core/hooks/route-guard.hook.ts
@@ -2,15 +2,26 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { TokenService } from "../services/token.service";
 import { ResponseUtil } from "@/shared/utils/response.util";
 
-const PUBLIC_ROUTES = ["/swagger", "/auth", "/api/health"];
+const PUBLIC_ROUTES = [
+  "/auth",
+  "/swagger",
+  "/api/health",
+  "flight/search",
+  "flight/airport",
+];
 
 export default async function RouteGuardHook(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
   try {
-    const isPublic = PUBLIC_ROUTES.some((route) => request.url.includes(route));
-    if (request.url === "/" || isPublic) return;
+    const pathWithoutQuery = request.url.split("?")[0];
+
+    const isPublic = PUBLIC_ROUTES.some((route) =>
+      pathWithoutQuery.includes(route)
+    );
+
+    if (pathWithoutQuery === "/" || isPublic) return;
 
     const session = await TokenService.verifyAndDecode(request, reply);
     if (session) request.user = session;
