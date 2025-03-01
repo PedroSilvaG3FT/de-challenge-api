cdff3fab304a84e5a57daafe857dc7f60d57f83b
Pedro Silva <pedro.silva-dev@hotmail.com>
2025-03-01 02:54:54 -0300

    feat: init logger to track actions

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -2,6 +2,7 @@ import {
   IAmadeusFlightOfferResponse,
   IAmadeusFlightOffersSearchParams,
 } from "amadeus";
+import logger from "@/shared/utils/logger.util";
 import { ResponseUtil } from "@/shared/utils/response.util";
 import { FlightHelper } from "@/shared/helpers/flight.helper";
 import { amadeusService } from "@/shared/amadeus/amadeus.service";
@@ -14,6 +15,7 @@ export class FlightService {
     if (response.statusCode === 200)
       return ResponseUtil.success(FlightHelper.buildFlights(response.result));
 
+    logger.error("Error on search flights", response?.data);
     return ResponseUtil.error(response.statusCode || 500, []);
   };
 }
